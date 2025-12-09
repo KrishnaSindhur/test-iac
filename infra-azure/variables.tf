46cb1fe2d84652fa56bf3563a2e69216cd02bc8a
KrishnaSindhur <krishna.sindhur@harness.io>
2025-12-09 14:18:02 +0530

    fix: test

--- infra-azure/variables.tf
+++ infra-azure/variables.tf
@@ -9,3 +9,13 @@ variable "vm_size" {
   type        = string
   default     = "Standard_B1s"
 }
+
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+}
