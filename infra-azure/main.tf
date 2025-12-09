af3068db572eb0c67bae935169fae5ae12bf69cd
KrishnaSindhur <krishna.sindhur@harness.io>
2025-12-09 17:04:09 +0530

    fix: test

--- infra-azure/main.tf
+++ infra-azure/main.tf
@@ -11,58 +11,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "opentofu-azure-demo-rg"
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "demo-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "demo-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "demo-nic"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "opentofu-azure-demo-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-  admin_password = "DemoPassword123!" # For demo only
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
+resource "azurerm_resource_group" "example" {
+  name     = "example-rg"
+  location = "East US"
 }
