c2c2510a2bcfef5f50dbedfe9d2098ac42c0205d
KrishnaSindhur <krishna.sindhur@harness.io>
2026-07-22 18:41:55 +0530

    added test sensistive

--- test-sensitive/variables.tf
+++ test-sensitive/variables.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 0.13"
 
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
+      version = ">= 1.7.0"
     }
   }
 }
